e634ffc38054ced59cc1c9d73f1f4c9d04b7e9b3
Georgi Dimitrov <austria1233@abv.bg>
2023-09-29 19:49:37 +0300

    new project

---
+++ src/pages/Shop/Shop.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { PRODUCTS } from '../../products'
+import { Product } from './Product'
+import './shop.css'
+
+
+export const Shop = () => {
+  return (
+    <div className='shop'>
+        <div className='ShopTitle'>
+            <h1>TechShop Co.</h1>
+        </div>
+        <div className='products'>
+            {
+                PRODUCTS.map((product)=>(<Product data={product } />))
+            }
+        </div>
+        
+    </div>
+  )
+}
---
+++ src/products.js
@@ -0,0 +1,49 @@
+export const PRODUCTS=[
+    {
+        id:1,
+        productName:"Iphone",
+        price:999.00,
+        productImage:'https://buybest.bg/storage/public/uploads/media-manager/app-modules-shop-models-product/1173/4095/iphone-14-pro-finish-select-202209-6-1inch-spaceblack.jpeg'
+    },{
+        id:2,
+        productName:"Samsung",
+        price:999.00,
+        productImage:'https://cdncloudcart.com/25880/products/images/42234/smartfon-samsung-galaxy-s23-ultra-12-gb-512-gb-lilav-63dacafa317f9_800x800.jpeg?1675283236'
+    }
+    ,{
+        id:3,
+        productName:"Lenovo Y700",
+        price:999.00,
+        productImage:'https://laptop.bg/system/images/104354/original/IdeaPad_Y700_17.jpg'
+    }
+    ,{
+        id:4,
+        productName:"Apple Mac laptop",
+        price:999.00,
+        productImage:'https://mediapark.uz/_next/image?url=https%3A%2F%2Fcdn.newmediapark.uz%2Fimages%2Fd60d65d2-41cd-49ed-b1ae-cdad529dad8c_test.jpg&w=1080&q=75'
+    }
+    ,{
+        id:5,
+        productName:"LG G6",
+        price:999.00,
+        productImage:'https://www.novtelefon.com/img/products/products_7308750_132962272lg-g6-4.jpg'
+    }
+    ,{
+        id:6,
+        productName:"Huawei P60 ",
+        price:999.00,
+        productImage:'https://media.a1.bg/newsite2018/private/img/images/devices-new-eshop/Huawei/Huawei%20P60%20Pro/p60-pro-profile-white.png'
+    }
+    ,{
+        id:7,
+        productName:"Samsung Z fold",
+        price:999.00,
+        productImage:'https://www.jarcomputers.com/images/custom/site/2022-08-11_15_30_49.jpg'
+    }
+    ,{
+        id:8,
+        productName:"Redmi Note 12",
+        price:999.00,
+        productImage:'https://www.xiaomishop.bg/image/cache/data/images/Redmi-Note-12-Pro-4G/Redmi-Note-12-4G-Onyx-Gray1-1000x1000.jpg'
+    }
+]
